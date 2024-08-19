34720af6f4791c22d09122e9ddba306f7408427b
sarwan <sarwanazhar12345@gmail.com>
2024-08-19 17:50:02 +0500

    redeploy

--- components/button-for-client.tsx
+++ components/button-for-client.tsx
@@ -14,6 +14,8 @@ export const ButtonServer = ({
 
     const { onOpen } = useModal()
 
+
+    //
     return (
         <button onClick={() => onOpen('play', {userId: profile.userId})} className="flex px-5 py-3 w-40 gap-3 justify-start text-white items-center rounded-md font-bold text-xl bg-green-600 hover:bg-green-800">
         <Image src='play.svg' alt="playImg" width={35} height={35} />
